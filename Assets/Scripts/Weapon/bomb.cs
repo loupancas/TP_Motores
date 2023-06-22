97bb09b8959f78806bed8bab4f4c6400c22f319f
loupancas <129621626+loupancas@users.noreply.github.com>
2023-06-22 10:23:36 -0500

    cambios

--- Assets/Scripts/Weapon/bomb.cs
+++ Assets/Scripts/Weapon/bomb.cs
@@ -6,6 +6,8 @@ public class bomb : MonoBehaviour
 {
     Rigidbody rgb;
     [SerializeField] float throwForce = 5f;
+    public bool isEnemyBomb;
+    public int damage;
     private void Awake()
     {
         rgb = GetComponent<Rigidbody>();
@@ -19,6 +21,49 @@ public class bomb : MonoBehaviour
         Destroy(this.gameObject, 2f);
     }
 
+    private void OnTriggerEnter(Collider collision) // las bullets se destruiran cuando colisionen con otros objetos que tengan collider, recordar que deben tener RigidBody
+    {
+        var damageable = collision.GetComponent<IBulletDamage>();
 
+        if (isEnemyBomb)
+        {
+            //var player = (Player)damageable; // convierte a player
+
+            if (damageable is Player)
+            {
+                var player = (Player)damageable;
+
+                if (player != null)
+                {
+                    if (damageable != null)
+                    {
+                        damageable.BulletDmg(damage);
+                    }
+
+                }
+            }
+
+
+
+
+        }
+        else
+        {
+            var enemy = (Enemy)damageable; // convierte a Enemy
+            if (enemy != null)
+            {
+                if (damageable != null)
+                {
+                    damageable.BulletDmg(damage);
+                }
+
+            }
+
+        }
+
+
+
+
+    }
 
 }
